18c12bde9d3f3dca07cee5e1b91dab241f437d93
PereWoulpy <laurent.turpin@insa-lyon.fr>
2018-10-12 14:40:02 +0200

    Init Commit

---
+++ wifinder-runner/src/main/java/insaif/rsdm/wifinder/WifinderApplication.java
@@ -0,0 +1,14 @@
+package insaif.rsdm.wifinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+public class WifinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WifinderApplication.class, args);
+    }
+}
---
+++ wifinder-runner/src/test/java/insaif/rsdm/wifinder/WifinderApplicationTests.java
@@ -0,0 +1,16 @@
+package insaif.rsdm.wifinder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WifinderApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
