75137f54ab98698be36a8de39cf537c0f3ea2381
Pyhro36 <pylou_11@hotmail.fr>
2018-10-19 16:15:31 +0200

    test enpoint in during

---
+++ wifinder-endpoint/src/java/ifinsa/rsdm/wifinder/endpoint/SampleController.java
@@ -0,0 +1,15 @@
+package ifinsa.rsdm.wifinder.endpoint;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+public class SampleController {
+
+    @RequestMapping(path="/greeting", method = GET)
+    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return name;
+    }
+}
--- wifinder-runner/src/main/java/insaif/rsdm/wifinder/runner/WifinderApplication.java
+++ wifinder-runner/src/main/java/insaif/rsdm/wifinder/runner/WifinderApplication.java
@@ -2,10 +2,9 @@ package insaif.rsdm.wifinder.runner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableJpaRepositories
 public class WifinderApplication {
 
     public static void main(String[] args) {
