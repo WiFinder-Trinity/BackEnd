4a1335cc25b781e8a8de2130788fbcb16b45d318
Quentin JODER <quentinjoder@gmail.com>
2018-11-11 14:58:31 +0100

    localhost:8080/swagger-ui.html

--- wifinder-runner/src/main/java/insaif/rsdm/wifinder/runner/WifinderApplication.java
+++ wifinder-runner/src/main/java/insaif/rsdm/wifinder/runner/WifinderApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
@@ -12,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
         "insaif.rsdm.wifinder.service"})
 @EntityScan(basePackages = "insaif.rsdm.wifinder.model")
 @EnableJpaRepositories(basePackages = "insaif.rsdm.wifinder.repository")
+@EnableSwagger2
 public class WifinderApplication {
 
     public static void main(String[] args) {
